4e10f534d319dc548a5fb9614a22179848f30a53
NicoRuizC <126577058+NicoRuizC@users.noreply.github.com>
2023-07-16 20:39:23 -0500

    Update Registro1.php

--- Registro1.php
+++ Registro1.php
@@ -1,33 +1,30 @@
 <?php
-// Obtener los datos del formulario
-$id_usuario = $_POST['id'];
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$confirm_password = $_POST['confirm_password'];
 
-// Validar la contraseña
-if ($password !== $confirm_password) {
-    die('Las contraseñas no coinciden');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+    $contraseña = $_POST['password'];
+
+   
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "conection_pane_cafe";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Error al conectar a la base de datos: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO usuario (nombre_u, apellido_u, contraseña, email_u) VALUES ('$nombre', '$apellido', '$contraseña', '$email')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Registro exitoso";
+    } else {
+        echo "Error al registrar usuario: " . $conn->error;
+    }
+
+    $conn->close();
 }
-
-  // conexion base de datos
-  $conn = mysqli_connect("localhost", "root", "","conection_pane_cafe" );
-                
-  // verificar conexion 
-  if (!$conn){
-    die("error al conectar a la base de datos:" . mysqli_connect_error());
-  }
-
-// Insertar los datos en la base de datos
-$sql = "INSERT INTO usuario (id_usuario, nombre_u, apellido_u, email_u, contraseña, confirm_password, idPago) VALUES ('$id_usuario','$nombre', '$apellido', '$email', '$password','$confirm_password',)";
-
-if ($conn->query($sql) === TRUE) {
-    echo 'Registro exitoso';
-} else {
-    echo 'Error al registrar: ' . $conn->error;
-}
-
-$conn->close();
 ?>
