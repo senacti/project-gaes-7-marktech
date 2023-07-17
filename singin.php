3a4614856acea6fd53f7b08b8a6b9c5a8985a5a2
NicoRuizC <126577058+NicoRuizC@users.noreply.github.com>
2023-07-16 20:41:02 -0500

    Update singin.php

--- singin.php
+++ singin.php
@@ -1,58 +1,41 @@
-
 <?php
-$servername = "localhost";  // Cambiar si el servidor MySQL no se encuentra en el localhost
+$servername = "127.0.0.1";
 $username = "root";
 $password = "";
-$database = "Panne_E_Cafee2";
+$dbname = "conection_pane_cafe";
 
-// Crear una conexión
-$conn = new mysqli($servername, $username, $password, $database);
 
-// Verificar si hay errores en la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error);
 }
 
-// Verificar si se envió el formulario
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Capturar los valores del formulario
-    $email = $_POST["correo"];
-    $password = $_POST["contraseña"];
 
-    // Aquí puedes realizar la consulta a la base de datos para verificar el inicio de sesión
-    // ...
-}
+$email = $_POST['floatingInput'];
+$password = $_POST['floatingPassword'];
+$usuario = $_POST['Usuario'];
 
-// ...
 
-// Consulta preparada
-$sql = "SELECT * FROM usuario WHERE correo = ? AND contraseña = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $email, $password);
-$stmt->execute();
-$result = $stmt->get_result();
+$sql = "SELECT * FROM usuario WHERE email_u = '$email' AND contraseña = '$password'";
+$result = $conn->query($sql);
 
-// Verificar el resultado de la consulta
-if ($result->num_rows == 1) {
-    // Inicio de sesión exitoso
-    // Redirigir a la página correspondiente según el tipo de usuario
-    $row = $result->fetch_assoc();
-    $tipoUsuario = $row["tipo_usuario"];
 
-    if ($tipoUsuario === "Cliente") {
-        header("Location: index.html");
-    } elseif ($tipoUsuario === "Vendedor") {
-        header("Location:ventas.html");
-    } elseif ($tipoUsuario === "Administrador") {
+if ($result->num_rows > 0) {
+    /
+    if ($usuario === "Cliente") {
+        header("Location: cliente.html");
+    } elseif ($usuario === "Vendedor") {
+        header("Location: dashboard_venta.html");
+    } elseif ($usuario === "Administrador") {
         header("Location: dashboard_admin.html");
     } else {
-        echo "Tipo de usuario desconocido.";
+        echo "Por favor, seleccione una opción de usuario.";
     }
 } else {
-    // Credenciales incorrectas
-    echo "Correo y/o contraseña incorrectos.";
+    echo "Credenciales inválidas. Por favor, inténtelo nuevamente.";
 }
 
-$stmt->close();
 $conn->close();
 ?>
