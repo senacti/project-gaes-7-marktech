bf1d9c16b2d08c72929c3c40ef4209b6ed467ff1
NicoRuizC <126577058+NicoRuizC@users.noreply.github.com>
2023-07-19 07:03:13 -0500

    Add files via upload

---
+++ Registro.php
@@ -0,0 +1,29 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+    $contraseña = $_POST['password'];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "conection_pane_cafe";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Error al conectar a la base de datos: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO usuario (nombre_u, apellido_u, contraseña, email_u) VALUES ('$nombre', '$apellido', '$contraseña', '$email')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Registro exitoso";
+    } else {
+        echo "Error al registrar usuario: " . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
